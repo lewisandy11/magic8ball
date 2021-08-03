e66e5ed904078bc653f194cb41fa466b114de86c
lewisandy11 <lewisandy11@gmail.com>
2021-08-03 14:29:43 -0500

    First working project

---
+++ Magic8ball.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Magic8ball {
+
+    public static void main(String[] args) {
+        System.out.println("What is thy question bitch");
+        Scanner scan = new Scanner(System.in);
+        scan.next();
+        scan.close();
+
+        Random rand = new Random();
+        int r = rand.nextInt(5);
+
+        if (r == 0) {
+            System.out.println("as i see it, yes");
+        } else if (r == 1) {
+            System.out.println("bro...no");
+        } else if (r == 2) {
+            System.out.println("Sure, fuck it");
+        } else if (r == 3) {
+            System.out.println("meh");
+        } else if (r == 4) {
+            System.out.println("ask your mother");
+        }
+
+    }
+
+}
